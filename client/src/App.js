9db5ca7919e388d6b76c8c244f75356e122de987
muhammadali28 <asafder902@gmail.com>
2022-01-23 18:03:34 +0500

    'deployed'

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Launch from './components/Launch';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const client = new ApolloClient({
-  uri:"http://localhost:5000/graphql"
+  uri:"/graphql"
 });
 
 function App() {
